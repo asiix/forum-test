fbae1d87874f138dff9cfba59e6f42a1a9627f91
asiix <taowangg13@gmail.com>
2024-02-05 16:19:11 +0100

    Changed API Routes

--- ForumThreads/obj/Debug/net8.0/ForumThreads.AssemblyInfo.cs
+++ ForumThreads/obj/Debug/net8.0/ForumThreads.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForumThreads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d48a8a7bad9174b10344f60fefa1aa09cb01b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e186fae54f05092d0b154ebf46ac156732a82d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForumThreads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForumThreads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
